8e214d3523a9f84d9b43a4ab868f6dee2b01d37e
Tobias Norton <tnorton20@yahoo.com>
2019-01-09 18:46:42 -0500

    Initial rails app created with Question and Answer models

---
+++ db/migrate/20190109234433_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :questions do |t|
+      t.string :header
+      t.text :body
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
